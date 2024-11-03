8d6ce8d43cde54d8dcbfd8cf97932667292666c4
Shreya Suman <shreyasuman003@gmail.com>
2024-11-03 17:13:43 +0530

    Updates

---
+++ precache-manifest.87fcff9248ba0516def94595af395db4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73face5be407f788ddb20985cbc7144c",
+    "url": "/DevOps_FHN_Assignment/index.html"
+  },
+  {
+    "revision": "26e5281d8ea5352c3960",
+    "url": "/DevOps_FHN_Assignment/static/js/2.4dc2145f.chunk.js"
+  },
+  {
+    "revision": "a1c2daf6b39b86e3b3a6",
+    "url": "/DevOps_FHN_Assignment/static/js/main.16d2e94f.chunk.js"
+  },
+  {
+    "revision": "7af7820e4689d1909ac6",
+    "url": "/DevOps_FHN_Assignment/static/js/runtime~main.afffe3a3.js"
+  }
+]);
